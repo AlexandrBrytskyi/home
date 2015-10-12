aff9d63e67553b49a0e86a8673598523ee3fe5c3
Brytskyi <brytskyi@mail.ua>
2015-10-12 19:14:51 +0300

    reflection serialise added

---
+++ src/reflection/MySerialiser.java
@@ -0,0 +1,17 @@
+package reflection;
+
+/**
+ * User: huyti
+ * Date: 11.10.15
+ */
+public class MySerialiser {
+
+    public static boolean write(Object obj, String path) {
+        return false;
+    }
+
+    public Object read(String path) {
+        return null;
+    }
+
+}
